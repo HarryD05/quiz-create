00704387f22d1822206bb829c28553ef1265257b
Harry Davis <harrytobydavis@gmail.com>
2020-09-26 21:31:33 +0100

    Initial commit: Project Setup

---
+++ client/src/app.js
@@ -0,0 +1,8 @@
+//React dependencies 
+import React from 'react';
+
+const App = () => {
+  return <h1>Hello World</h1>
+}
+
+export default App;
---
+++ client/src/index.js
@@ -0,0 +1,14 @@
+//React dependencies
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+//Components
+import App from './app';
+
+//Styling
+import './index.scss';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
