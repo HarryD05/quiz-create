4a8f78c8ac77ee54e045b4e1bc6065866d0e5d53
Harry Davis <harrytobydavis@gmail.com>
2021-06-10 17:15:03 +0100

    Added teacher homepage info for help button

--- client/src/components/navbar/infoText.js
+++ client/src/components/navbar/infoText.js
@@ -17,7 +17,31 @@ const getInfoText = path => {
 
     case '/teacher/home':
       title = 'Teacher homepage';
-      content = (<p>Coming soon...</p>);
+      content = (<ul>
+        <li>There are 3 parts to the teacher homepage: the classes section, the student section and the create a class section.</li>
+        <li>Your Classes</li>
+        <ul>
+          <li>Use the dropdown menu above the table to select one of your classes.</li>
+          <li>The table below will update with the data linked to the selected class.</li>
+          <li>The percentage next to the poorest/best topic is the average score this class achieves on questions of that topic.</li>
+          <li>The 'see all assignments' button will bring up a modal displaying each assignment you have for the selected class which outlines the due date, average score achieved, students who haven't completed the assignment, students who have completed the assignment (and their mark) and each question is displayed with the average mark achieved.</li>
+          <li>The 'set new assignment' button will redirect you to the quiz creation page of the website.</li>
+        </ul>
+        <li>Your Students</li>
+        <ul>
+          <li>Use the dropdown menu above the table to select one of your classes.</li>
+          <li>The table below will update with the data linked to the selected student.</li>
+          <li>The percentage next to the poorest/best topic is the average score this student achieves on questions of that topic.</li>
+          <li>The 'see all student's assignments' button will bring up a modal displaying each assignment you have set for this student which whether or not they have completed the assignment, their score, the time it took them (if you set the assignment to record the time), on which questions they used hints and their answers.</li>
+        </ul>
+        <li>Create a new class</li>
+        <ul>
+          <li>Below the 2 tables there is a form/input area to create a new class.</li>
+          <li>There are 4 inputs needed to create a new class: class name, qualification (KS3, GCSE, A-level etc.), subject and joining code (which has to be unique so if it is already taken you will be asked try to again).</li>
+          <li>You will then see a modal once you press create class stating whether the process of creating the class was successful.</li>
+          <li>In order for students to join the class you will need to share the joining code with them.</li>
+        </ul>
+      </ul>);
       break;
 
     default:
