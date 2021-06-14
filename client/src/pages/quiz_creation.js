a5552dfce610713d88b7a5bdd345f3fa0a3bf8d6
Harry Davis <harrytobydavis@gmail.com>
2021-06-14 15:13:53 +0100

    Added temporary alert statement

--- client/src/pages/quiz_creation.js
+++ client/src/pages/quiz_creation.js
@@ -7,6 +7,8 @@ const QuizCreation = props => {
     <div id="quiz-creation">
       <h2>Quiz creation page</h2>
       <p>This page with have the quiz creation UI</p>
+
+      {alert('This is the quiz creation page, this hasn\'t been developed yet, if you press the home button at the top left of the page you will be redirected back to the homepage')}
     </div>
   );
 }
